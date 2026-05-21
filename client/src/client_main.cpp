44a1c6817db34d51f59264150807542d3082835e
chri <chrimctech@gmail.com>
2026-05-21 11:10:55 +0200

    wip

--- client/src/client_main.cpp
+++ client/src/client_main.cpp
@@ -1,1 +1,10 @@
 #include "client_main.h"
+#include <raylib.h>
+
+int main() {
+    InitWindow(1280, 720, "test");
+
+    while (!WindowShouldClose()) {
+        ClearBackground({100, 150, 200});
+    }
+}
